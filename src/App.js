24e74f10f65dd9a444ac615bb3f2b28e31dd4970
Caio Carvalho <kaiogesteira@gmail.com>
2023-03-17 17:28:47 -0300

    Sign Up system working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { GlobalStyle } from "./GlobalStyles";
+import { GlobalStyle } from "./styles/GlobalStyles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 export default function App() {
+  
   return (
-    <BrowserRouter>
-      <GlobalStyle />
+      <BrowserRouter>
+        <GlobalStyle />
 
-      <Routes>
-        <Route path="/" element={<LoginPage />}/>
-        <Route path="/cadastro" element={<RegisterPage />}/>
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}/>
+          <Route path="/cadastro" element={<RegisterPage />}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
   
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,23 +1,79 @@
-import { Container, UserForm, UnderlineText } from './styled';
-import logo from '../../assets/imgs/logo.png';
-import { Link } from 'react-router-dom';
+import { Container, UserForm, UnderlineText } from "./styled";
+import logo from "../../assets/imgs/logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { SIGN_UP } from "../../constants/urls";
+import axios from "axios";
 
-export default function LoginPage() {
-    return (
-        <Container>
-            <img src={logo} alt="logo" />
+export default function RegisterPage() {
+	const navigate = useNavigate();
 
-            <UserForm>
-                <input type="email" placeholder="email" required/>
-                <input type="password" placeholder="senha" required/>
-                <input type="text" placeholder="nome" required/>
-                <input type="url" placeholder="foto" required/>
-                <button type="submit">Cadastrar</button>
-            </UserForm>
+  // Object that contains the information of the form to be used later on the submition
+  const [form, setForm] = useState({
+    email: "",
+    name: "",
+    image: "",
+    password: "",
+  });
 
-            <Link to={'/'}>
-                <UnderlineText>Já tem uma conta? Faça login!</UnderlineText>
-            </Link>
-        </Container> 
-    );
+	// Takes the information of the form starte and updates with the new information the user typed
+  function handleChange(e) {
+		setForm({...form, [e.target.name]: e.target.value });
+	}
+
+	// Creates the body object and send the information to the backend in order to create a new user
+	function handleSubmit(e) {
+		e.preventDefault();
+		const body = {...form};
+
+		const promisse = axios.post(SIGN_UP, body);
+		promisse.then(response => navigate("/"));
+		promisse.catch(error => console.log(error));
+	}
+
+  return (
+    <Container>
+      <img src={logo} alt="logo" />
+
+      <UserForm onSubmit={handleSubmit}>
+        <input 
+					type="email" 
+					placeholder="email"
+					name={'email'} 
+					required 
+					value={form.email} 
+					onChange={handleChange}
+				/>
+        <input
+          type="password"
+          placeholder="senha"
+					name={'password'} 
+          required
+          value={form.password}
+					onChange={handleChange}
+        />
+        <input 
+					type="text" 
+					placeholder="nome"
+					name={'name'}  
+					required 
+					value={form.name} 
+					onChange={handleChange}
+				/>
+        <input 
+					type="url" 
+					placeholder="foto"
+					name={'image'}  
+					required 
+					value={form.image}
+					onChange={handleChange} 
+				/>
+        <button type="submit">Cadastrar</button>
+      </UserForm>
+
+      <Link to={"/"}>
+        <UnderlineText>Já tem uma conta? Faça login!</UnderlineText>
+      </Link>
+    </Container>
+  );
 }
